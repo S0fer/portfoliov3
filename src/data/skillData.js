801924046b61f25fabfaac04bc734523a96c3cdc
João Pedro Alarcón Duarte <joaoad1099@gmail.com>
2022-09-22 22:17:30 -0300

    skilldata fix

--- src/data/skillData.js
+++ src/data/skillData.js
@@ -1,86 +1,124 @@
+import react from "../images/skills/react.svg";
+import angler from "../images/animals/angler.svg";
+import html from "../images/skills/html.svg";
+import bfish from "../images/animals/bfish.svg";
+import css from "../images/skills/css.svg";
+import dori from "../images/animals/dori.svg";
+import github from "../images/skills/github.svg";
+import gfish from "../images/animals/gfish.svg";
+import git from "../images/skills/git.svg";
+import prawn from "../images/animals/prawn.svg";
+import vscode from "../images/skills/vscode.svg";
+import shark from "../images/animals/shark.svg";
+import python from "../images/skills/python.svg";
+import squid from "../images/animals/squid.svg";
+import mui from "../images/skills/mui.svg";
+import starfish from "../images/animals/starfish.svg";
+import js from "../images/skills/js.svg";
+import tfish from "../images/animals/tfish.svg";
+import ts from "../images/skills/ts.svg";
+import turtle from "../images/animals/turtle.svg";
+import csharp from "../images/skills/csharp.svg";
+import whale from "../images/animals/whale.svg";
+import selenium from "../images/skills/selenium.svg";
+import yfish from "../images/animals/yfish.svg";
+
+
 const skillData = [
     {
         index: 0,
         name: "Reactjs",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+        icon: react,
+        animal: angler,
         level: "80%",
     },
     {
         index: 1,
         name: "HTML5",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+        icon: html,
+        animal: bfish,
         level: "90%",
     },
     {
         index: 2,
         name: "CSS3",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+        icon: css,
+        animal: dori,
         level: "80%",
     },
     {
         index: 3,
         name: "Github",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+        icon: github,
+        animal: gfish,
         level: "80%",
     },
     {
         index: 4,
         name: "Git",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+        icon: git,
+        animal: prawn,
         level: "60%",
     },
     {
         index: 5,
         name: "Visual Studio Code",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+        icon: vscode,
+        animal: shark,
         level: "100%",
     },
     {
         index: 6,
         name: "Python",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        icon: python,
+        animal: squid,
         level: "60%",
     },
     {
         index: 7,
         name: "Material UI",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+        icon: mui,
+        animal: starfish,
         level: "40%",
     },
     {
         index: 8,
         name: "Javascript",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        icon: js,
+        animal: tfish,
         level: "100%",
     },
     {
         index: 9,
         name: "Typescript",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        icon: ts,
+        animal: turtle,
         level: "40%",
     },
     {
         index: 10,
         name: "C#",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+        icon: csharp,
+        animal: whale,
         level: "40%",
     },
     {
         index: 11,
         name: "Selenium",
         description:"",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/selenium/selenium-original.svg",
+        icon: selenium,
+        animal: yfish,
         level: "40%",
     }
 ]
